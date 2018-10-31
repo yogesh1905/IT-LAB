4fe479242e1c77ea2804e113ab551a7686321256
Mukesh-BR <32503710+Mukesh-BR@users.noreply.github.com>
2018-10-31 10:38:02 +0530

    ated o

---
+++ idk.py
@@ -0,0 +1,31 @@
+def strobogrammatic_num(n):
+     
+    result = numdef(n, n)
+    return result
+     
+# definition function
+def numdef(n, length):
+     
+    if n == 0: return [""]
+    if n == 1: return ["1", "0", "8"]
+     
+    middles = numdef(n - 2, length)
+    print(middles)
+    result = []
+     
+    for middle in middles:
+        if n != length:            
+            result.append("0" + middle + "0")
+ 
+        result.append("8" + middle + "8")
+        result.append("1" + middle + "1")
+        result.append("9" + middle + "6")
+        result.append("6" + middle + "9")
+    return result
+ 
+# Driver Code
+if __name__ == '__main__':
+     
+    # Print all Strobogrammatic 
+    # number for n = 3
+    print(strobogrammatic_num(6))
